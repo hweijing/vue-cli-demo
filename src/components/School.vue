1aff73540462b7201c209965da9f73bd5a7f8813
hweijing <jh13290900214@163.com>
2024-06-04 02:15:49 +0800

    2024.6.4

---
+++ src/components/School.vue
@@ -0,0 +1,33 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="demo">
+    <h1>学校名称：{{ name }}</h1>
+    <h1>学校地址：{{ address }}</h1>
+    <button @click="showName">点我提示学校名</button>
+  </div>
+</template>
+
+<!-- 组件交互相关的代码（数据、方法等） -->
+<script>
+// 当暴露的东西是一个的时候，用默认暴露会简化语法
+export default {
+  data() {
+    return {
+      name: "tsing",
+      address: "beijing",
+    };
+  },
+  methods: {
+    showName() {
+      alert(this.name);
+    },
+  },
+};
+</script>
+
+<!-- 组件的样式 -->
+<style>
+.demo {
+  background-color: aquamarine;
+}
+</style>
---
+++ src/components/Student.vue
@@ -0,0 +1,21 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <h1>学生姓名：{{ name }}</h1>
+    <h1>学生年龄：{{ age }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "zhangsan",
+      age: 18,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
